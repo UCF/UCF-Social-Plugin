eb8da7040ae88ff83550eb33aea597eb4601f1e0
RJ Bruneel <rj.bruneel@ucf.edu>
2017-06-28 11:56:38 -0400

    Added social links shortcode

--- includes/ucf-social-shortcode.php
+++ includes/ucf-social-shortcode.php
@@ -9,7 +9,7 @@
 
 if ( ! class_exists( 'UCF_Social_Shortcode' ) ) {
 	class UCF_Social_Shortcode {
-		public static function shortcode( $atts ) {
+		public static function icons_shortcode( $atts ) {
 			$atts = shortcode_atts( array(
 				'color'  => 'color',
 				'size'   => 'md'
@@ -17,6 +17,16 @@ if ( ! class_exists( 'UCF_Social_Shortcode' ) ) {
 
 			return UCF_Social_Common::display_social_icons( $atts );
 		}
+
+		public static function links_shortcode( $atts ) {
+			$atts = shortcode_atts( array(
+				'size'   => 'sm'
+			), $atts, 'ucf-social-links' );
+
+			return UCF_Social_Common::display_social_links( $atts );
+		}
 	}
-	add_shortcode( 'ucf-social-icons', array( 'UCF_Social_Shortcode', 'shortcode' ) );
+
+	add_shortcode( 'ucf-social-icons', array( 'UCF_Social_Shortcode', 'icons_shortcode' ) );
+	add_shortcode( 'ucf-social-links', array( 'UCF_Social_Shortcode', 'links_shortcode' ) );
 }
